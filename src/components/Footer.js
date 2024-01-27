c55154151f966b6487d9424d33a49f13dfe5aeed
phillip <thoopra1@gmail.com>
2024-01-27 08:12:37 -0700

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import LinkedInIcon from "@mui/icons-material/LinkedIn"
+// Tags can self close when nothing goes inside.
 function Footer() {
   return (
-    <div>
-      "Testing footer"
+    <div className='footer'>
+      <div className = "socialMedia">
+        <LinkedInIcon /> 
+      </div>
+      <p> &copy; 2024 philliptran.io</p>
+
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import backgroundImage from "../assets/r34.jpg"
 import "../styles/Home.css";
+import "../styles/Footer.css";
+
 
 function Home() {
   return (
-    <div className = "home">
-      <div 
-      className = "headerContainer" 
-      style={{ backgroundImage: `url(${backgroundImage})`}}>
+    <div className = "home" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: '40%', backgroundPosition: 'right bottom'}}>
+      <div className = "headerContainer" >
         <h1>Phillip Tran</h1>
         <p>My Portfolio</p> 
         <Link to ="contact">
@@ -20,3 +20,5 @@ function Home() {
 }
 
 export default Home
+
+// npm start
